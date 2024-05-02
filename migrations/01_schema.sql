7ca47e6dd1cf04ebf3e6985345e11b8b4f0f9c80
Miguel Augusto <miguelmasche@gmail.com>
2024-05-01 19:04:01 -0700

    add all tables

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -1,46 +1,50 @@
--- DROP TABLE IF EXISTS lightbnb CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS properties CASCADE;
+DROP TABLE IF EXISTS reservations CASCADE;
+DROP TABLE IF EXISTS property_reviews CASCADE;
 
 CREATE TABLE users (
-	id serial PRIMARY KEY NOT NULL,
-	name varchar(255) NOT NULL,
-	email varchar(255) NOT NULL,
-	password varchar(255) NOT NULL
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
 );
 
+CREATE TABLE properties (
+  id SERIAL PRIMARY KEY NOT NULL,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
 
--- CREATE TABLE properties (
--- 	id serial pk increments unique
--- 	owner_id integer > users.id
--- 	title varchar(255)
--- 	description text
--- 	cost_per_night integer
--- 	parking_spaces integer
--- 	number_of_bathrooms integer
--- 	number_of_bedrooms integer
--- 	thumbnail_url integer
--- 	cover_photo_url integer
--- 	country varchar(255)
--- 	street integer
--- 	province varchar(20)
--- 	post_code varchar(10)
--- 	is_active boolean
--- );
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  thumbnail_photo_url VARCHAR(255) NOT NULL,
+  cover_photo_url VARCHAR(255) NOT NULL,
+  cost_per_night INTEGER  NOT NULL DEFAULT 0,
+  parking_spaces INTEGER  NOT NULL DEFAULT 0,
+  number_of_bathrooms INTEGER  NOT NULL DEFAULT 0,
+  number_of_bedrooms INTEGER  NOT NULL DEFAULT 0,
 
+  country VARCHAR(255) NOT NULL,
+  street VARCHAR(255) NOT NULL,
+  city VARCHAR(255) NOT NULL,
+  province VARCHAR(255) NOT NULL,
+  post_code VARCHAR(255) NOT NULL,
 
--- CREATE TABLE reservations (
--- 	id int pk increments unique
--- 	start_date date
--- 	end_date date
--- 	property_id int *> properties.id
--- 	guest_id int > users.id
--- )
+  active BOOLEAN NOT NULL DEFAULT TRUE
+);
 
--- CREATE TABLE property_reviews (
---   property_reviews
--- 	id integer pk increments unique
--- 	guest_id integer > users.id
--- 	property_id integer
--- 	reservation_id integer
--- 	message text
--- 	rating integer
--- )
+CREATE TABLE reservations (
+  id SERIAL PRIMARY KEY NOT NULL,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE property_reviews (
+  id SERIAL PRIMARY KEY NOT NULL,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
+  rating SMALLINT NOT NULL DEFAULT 0,
+  message TEXT
+);
